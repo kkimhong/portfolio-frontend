0a78de7a4678d74ce110002ac53b09dc2972bc56
k.hong <kruykimhongkkh@gmail.com>
2026-02-07 14:58:32 +0700

    change sth in providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,11 @@
 "use client";
+
 import { Spinner } from "@/components/ui/spinner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 import React, { useState } from "react";
 
+// define the props type inline or as an interface
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
