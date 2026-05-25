5ef2c7a62fbdbbc976172a3b613eb01f048fcf45
k.kimhong <kruykimhongkkh@gmail.com>
2026-05-25 17:07:16 +0700

    remove unuse file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
-import "./globals.css";
 import Providers from "./providers";
 
 const jetbrainsMono = JetBrains_Mono({
